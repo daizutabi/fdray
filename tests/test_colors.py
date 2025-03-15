ba7ab6c1114d99b2739b02a9e4620368e2f4f5bb
Daizu <daizutabi@gmail.com>
2025-03-15 23:04:34 +0000

    Add tests for Color and Background classes, including error handling and RGBA support

--- tests/test_colors.py
+++ tests/test_colors.py
@@ -1,4 +1,6 @@
-from fdray.colors import Color
+import pytest
+
+from fdray.colors import Background, Color
 
 
 def test_color_str():
@@ -16,11 +18,36 @@ def test_color_str_alpha():
     assert str(color) == "rgbt <0, 0.502, 0, 0.3>"
 
 
+def test_color_str_rgba():
+    color = Color("#10203040", pigment=False)
+    assert str(color) == "rgbt <0.0627, 0.125, 0.188, 0.749>"
+
+
 def test_color_float():
-    color = Color((0.2, 0.3, 0.4), pigment=False)
-    assert str(color) == "rgb <0.2, 0.3, 0.4>"
+    color = Color((0.2, 0.3, 0.4), pigment=True)
+    assert str(color) == "pigment { rgb <0.2, 0.3, 0.4> }"
 
 
 def test_color_float_alpha():
     color = Color((0.2, 0.3, 0.4), 0.2, pigment=False)
     assert str(color) == "rgbt <0.2, 0.3, 0.4, 0.8>"
+
+
+def test_color_float_rgba():
+    color = Color((0.2, 0.3, 0.4, 0.6), pigment=True)
+    assert str(color) == "pigment { rgbt <0.2, 0.3, 0.4, 0.4> }"
+
+
+def test_color_str_error():
+    with pytest.raises(ValueError):
+        Color("invalid")
+
+
+def test_color_float_error():
+    with pytest.raises(ValueError):
+        Color((0.2, 0.3, 0.4, 0.6, 0.8), pigment=True)  # type: ignore
+
+
+def test_background_str():
+    background = Background("blue")
+    assert str(background) == "background { rgb <0, 0, 1> }"
