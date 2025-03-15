84b31a3aa616dd180287494390fc68e9e0f7e957
Daizu <daizutabi@gmail.com>
2025-03-15 23:11:43 +0000

    Add unit tests for Transform and LightSource classes to validate string representation

---
+++ tests/test_attributes.py
@@ -0,0 +1,19 @@
+def test_attribute_str():
+    from fdray.attributes import Transform
+
+    x = Transform(scale=0.5)
+    assert str(x) == "transform { scale 0.5 }"
+
+
+def test_attribute_missing():
+    from fdray.scene import LightSource
+
+    x = LightSource((1, 2, 3))
+    assert str(x) == "light_source { <1, 2, 3> }"
+
+
+def test_attribute_bool():
+    from fdray.scene import LightSource
+
+    x = LightSource((1, 2, 3), shadowless=True)
+    assert str(x) == "light_source { <1, 2, 3> shadowless }"
