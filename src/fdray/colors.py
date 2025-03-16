a772ec139b52105c011fb6b89948a8702479d6fb
Daizu <daizutabi@gmail.com>
2025-03-16 12:07:37 +0000

    Add a color palette to the colors module for improved color management.

--- src/fdray/colors.py
+++ src/fdray/colors.py
@@ -254,3 +254,16 @@ cnames = {
     "yellow": "#FFFF00",
     "yellowgreen": "#9ACD32",
 }
+
+COLOR_PALETTE = [
+    "#1f77b4",
+    "#ff7f0e",
+    "#2ca02c",
+    "#d62728",
+    "#9467bd",
+    "#8c564b",
+    "#e377c2",
+    "#7f7f7f",
+    "#bcbd22",
+    "#17becf",
+]
