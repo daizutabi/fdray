7e774c4e6001393172a76dc945e017478dea9b6a
Daizu <daizutabi@gmail.com>
2025-03-21 23:59:55 +0000

    Add detailed docstrings for Color class and related functions; enhance color representation and string formatting for alpha, filter, and transmit properties.

--- tests/test_color.py
+++ tests/test_color.py
@@ -26,9 +26,14 @@ def test_color_str_include_color_false():
     assert str(color) == "rgb <1, 0, 0>"
 
 
-def test_color_str_filter():
+def test_color_str_alpha():
     color = Color("green", 0.7)
-    assert str(color) == "color rgbf <0, 0.502, 0, 0.7>"
+    assert str(color) == "color rgbt <0, 0.502, 0, 0.3>"
+
+
+def test_color_str_rgba():
+    color = Color("#10203040", transmit=0.7)
+    assert str(color) == "color rgbt <0.0627, 0.125, 0.188, 0.749>"
 
 
 def test_color_str_transmit():
@@ -46,15 +51,25 @@ def test_color_tuple():
     assert str(color) == "color rgb <0.2, 0.3, 0.4>"
 
 
+def test_color_tuple_alpha():
+    color = Color((0.2, 0.3, 0.4), alpha=0.2)
+    assert str(color) == "color rgbt <0.2, 0.3, 0.4, 0.8>"
+
+
+def test_color_tuple_filter():
+    color = Color((0.2, 0.3, 0.4), filter=0.2)
+    assert str(color) == "color rgbf <0.2, 0.3, 0.4, 0.2>"
+
+
+def test_color_tuple_rgba():
+    color = Color((0.2, 0.3, 0.4, 0.2))
+    assert str(color) == "color rgbt <0.2, 0.3, 0.4, 0.8>"
+
+
 def test_color_color():
     color = Color((0.2, 0.3, 0.4), filter=0.2, transmit=0.8)
     color = Color(color, filter=0.3, transmit=0.1)
     assert str(color) == "color rgbft <0.2, 0.3, 0.4, 0.3, 0.1>"
-
-
-def test_color_tuple_alpha():
-    color = Color((0.2, 0.3, 0.4), alpha=0.2)
-    assert str(color) == "color rgbt <0.2, 0.3, 0.4, 0.8>"
 
 
 def test_background_str():
