76f41ddd2271899843e78bc5074fb893e7a748ea
Daizu <daizutabi@gmail.com>
2025-03-21 01:24:32 +0000

    Add to_str function for improved string conversion; update convert function to utilize to_str for tuples and lists.

--- src/fdray/utils.py
+++ src/fdray/utils.py
@@ -9,9 +9,16 @@ if TYPE_CHECKING:
 def convert(arg: Any) -> str:
     if isinstance(arg, tuple):
         if len(arg) == 2:
-            return f"{arg[0]} {arg[1]}"
+            return f"{to_str(arg[0])} {to_str(arg[1])}"
 
-        arg = ", ".join(str(x) for x in arg)
+        arg = ", ".join(to_str(x) for x in arg)
         return f"<{arg}>"
+
+    return str(arg)
+
+
+def to_str(arg: Any) -> str:
+    if isinstance(arg, float):
+        return f"{arg:.5g}"
 
     return str(arg)
