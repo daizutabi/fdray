d69e2ebc08e6570b7f44a8d81cee2c9896c37f67
Daizu <daizutabi@gmail.com>
2025-03-29 22:39:59 +0000

    Add color encoding functions and error handling for matplotlib import; enhance tests for colormap and direction encoding.

--- src/fdray/data/color.py
+++ src/fdray/data/color.py
@@ -1,8 +1,20 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 import numpy as np
 
 from fdray.core.color import Color
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+    from numpy.typing import NDArray
+
+
+def raise_import_error(msg: str) -> None:
+    msg = f"{msg} Install with: pip install matplotlib."
+    raise ImportError(msg) from None
 
 
 def get_colormap(name: str, num_colors: int = 256) -> list[Color]:
@@ -17,13 +29,41 @@ def get_colormap(name: str, num_colors: int = 256) -> list[Color]:
     """
     try:
         import matplotlib.pyplot as plt
-
-        cmap = plt.get_cmap(name)
-        return [Color(cmap(i)[:3]) for i in np.linspace(0, 1, num_colors)]
-
     except ImportError:  # no cov
-        msg = (
-            f"Colormap '{name}' requires matplotlib. "
-            "Install with: pip install matplotlib."
-        )
-        raise ImportError(msg) from None
+        raise_import_error(f"Colormap '{name}' requires matplotlib.")
+
+    cmap = plt.get_cmap(name)
+    return [Color(cmap(i)[:3]) for i in np.linspace(0, 1, num_colors)]
+
+
+def encode_direction_field(
+    field: Sequence | NDArray,
+    axis: int = 2,
+) -> NDArray[np.float64]:
+    try:
+        from matplotlib.colors import hsv_to_rgb
+    except ImportError:  # no cov
+        raise_import_error("HSV to RGB conversion requires matplotlib.")
+
+    if not isinstance(field, np.ndarray):
+        field = np.array(field, dtype=np.float64)
+
+    i, j = [[1, 2], [0, 2], [0, 1]][axis]
+    x = field[..., i]
+    y = field[..., j]
+    z = field[..., axis]
+    geom = np.linalg.norm(field, axis=-1) != 0
+
+    hsv = np.empty_like(field)
+    hsv[..., 0] = (np.arctan2(-y, -x) + np.pi) / (2 * np.pi)
+    hsv[..., 1] = np.where(geom, 1 - z**2, 0)
+    hsv[..., 2] = 1 - (1 - z) ** 2 / 4
+
+    return hsv_to_rgb(hsv)
+
+
+def encode_direction(vector: Sequence, axis: int = 2) -> Color:
+    norm = np.linalg.norm(vector)
+    field = [[x / norm for x in vector]]
+    color = encode_direction_field(field, axis)
+    return Color(color[0])
