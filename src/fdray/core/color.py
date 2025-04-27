117b7fa407547c59b28d9922b4a266ba8a163152
Daizu <daizutabi@gmail.com>
2025-04-27 22:19:09 +0000

    Refactor color handling by introducing ColorName enumeration for web colors and updating rgb function to support lowercase color names.

--- src/fdray/core/color.py
+++ src/fdray/core/color.py
@@ -14,6 +14,7 @@ common web color standards.
 
 from __future__ import annotations
 
+from enum import StrEnum
 from typing import TYPE_CHECKING
 
 from fdray.data.color import colorize_direction
@@ -200,7 +201,8 @@ def rgb(color: str) -> RGB | str:
         >>> rgb("#00FF00FF")  # Alpha component is ignored
         (0.0, 1.0, 0.0)
     """
-    color = cnames.get(color, color)
+    if color.islower():
+        color = getattr(ColorName, color.upper(), color)
 
     if not isinstance(color, str) or not color.startswith("#") or len(color) < 7:
         return color
@@ -209,156 +211,158 @@ def rgb(color: str) -> RGB | str:
     return int(r, 16) / 255, int(g, 16) / 255, int(b, 16) / 255
 
 
-cnames = {
-    "aliceblue": "#F0F8FF",
-    "antiquewhite": "#FAEBD7",
-    "aqua": "#00FFFF",
-    "aquamarine": "#7FFFD4",
-    "azure": "#F0FFFF",
-    "beige": "#F5F5DC",
-    "bisque": "#FFE4C4",
-    "black": "#000000",
-    "blanchedalmond": "#FFEBCD",
-    "blue": "#0000FF",
-    "blueviolet": "#8A2BE2",
-    "brown": "#A52A2A",
-    "burlywood": "#DEB887",
-    "cadetblue": "#5F9EA0",
-    "chartreuse": "#7FFF00",
-    "chocolate": "#D2691E",
-    "coral": "#FF7F50",
-    "cornflowerblue": "#6495ED",
-    "cornsilk": "#FFF8DC",
-    "crimson": "#DC143C",
-    "cyan": "#00FFFF",
-    "darkblue": "#00008B",
-    "darkcyan": "#008B8B",
-    "darkgoldenrod": "#B8860B",
-    "darkgray": "#A9A9A9",
-    "darkgreen": "#006400",
-    "darkgrey": "#A9A9A9",
-    "darkkhaki": "#BDB76B",
-    "darkmagenta": "#8B008B",
-    "darkolivegreen": "#556B2F",
-    "darkorange": "#FF8C00",
-    "darkorchid": "#9932CC",
-    "darkred": "#8B0000",
-    "darksalmon": "#E9967A",
-    "darkseagreen": "#8FBC8F",
-    "darkslateblue": "#483D8B",
-    "darkslategray": "#2F4F4F",
-    "darkslategrey": "#2F4F4F",
-    "darkturquoise": "#00CED1",
-    "darkviolet": "#9400D3",
-    "deeppink": "#FF1493",
-    "deepskyblue": "#00BFFF",
-    "dimgray": "#696969",
-    "dimgrey": "#696969",
-    "dodgerblue": "#1E90FF",
-    "firebrick": "#B22222",
-    "floralwhite": "#FFFAF0",
-    "forestgreen": "#228B22",
-    "fuchsia": "#FF00FF",
-    "gainsboro": "#DCDCDC",
-    "ghostwhite": "#F8F8FF",
-    "gold": "#FFD700",
-    "goldenrod": "#DAA520",
-    "gray": "#808080",
-    "green": "#008000",
-    "greenyellow": "#ADFF2F",
-    "grey": "#808080",
-    "honeydew": "#F0FFF0",
-    "hotpink": "#FF69B4",
-    "indianred": "#CD5C5C",
-    "indigo": "#4B0082",
-    "ivory": "#FFFFF0",
-    "khaki": "#F0E68C",
-    "lavender": "#E6E6FA",
-    "lavenderblush": "#FFF0F5",
-    "lawngreen": "#7CFC00",
-    "lemonchiffon": "#FFFACD",
-    "lightblue": "#ADD8E6",
-    "lightcoral": "#F08080",
-    "lightcyan": "#E0FFFF",
-    "lightgoldenrodyellow": "#FAFAD2",
-    "lightgray": "#D3D3D3",
-    "lightgreen": "#90EE90",
-    "lightgrey": "#D3D3D3",
-    "lightpink": "#FFB6C1",
-    "lightsalmon": "#FFA07A",
-    "lightseagreen": "#20B2AA",
-    "lightskyblue": "#87CEFA",
-    "lightslategray": "#778899",
-    "lightslategrey": "#778899",
-    "lightsteelblue": "#B0C4DE",
-    "lightyellow": "#FFFFE0",
-    "lime": "#00FF00",
-    "limegreen": "#32CD32",
-    "linen": "#FAF0E6",
-    "magenta": "#FF00FF",
-    "maroon": "#800000",
-    "mediumaquamarine": "#66CDAA",
-    "mediumblue": "#0000CD",
-    "mediumorchid": "#BA55D3",
-    "mediumpurple": "#9370DB",
-    "mediumseagreen": "#3CB371",
-    "mediumslateblue": "#7B68EE",
-    "mediumspringgreen": "#00FA9A",
-    "mediumturquoise": "#48D1CC",
-    "mediumvioletred": "#C71585",
-    "midnightblue": "#191970",
-    "mintcream": "#F5FFFA",
-    "mistyrose": "#FFE4E1",
-    "moccasin": "#FFE4B5",
-    "navajowhite": "#FFDEAD",
-    "navy": "#000080",
-    "oldlace": "#FDF5E6",
-    "olive": "#808000",
-    "olivedrab": "#6B8E23",
-    "orange": "#FFA500",
-    "orangered": "#FF4500",
-    "orchid": "#DA70D6",
-    "palegoldenrod": "#EEE8AA",
-    "palegreen": "#98FB98",
-    "paleturquoise": "#AFEEEE",
-    "palevioletred": "#DB7093",
-    "papayawhip": "#FFEFD5",
-    "peachpuff": "#FFDAB9",
-    "peru": "#CD853F",
-    "pink": "#FFC0CB",
-    "plum": "#DDA0DD",
-    "powderblue": "#B0E0E6",
-    "purple": "#800080",
-    "rebeccapurple": "#663399",
-    "red": "#FF0000",
-    "rosybrown": "#BC8F8F",
-    "royalblue": "#4169E1",
-    "saddlebrown": "#8B4513",
-    "salmon": "#FA8072",
-    "sandybrown": "#F4A460",
-    "seagreen": "#2E8B57",
-    "seashell": "#FFF5EE",
-    "sienna": "#A0522D",
-    "silver": "#C0C0C0",
-    "skyblue": "#87CEEB",
-    "slateblue": "#6A5ACD",
-    "slategray": "#708090",
-    "slategrey": "#708090",
-    "snow": "#FFFAFA",
-    "springgreen": "#00FF7F",
-    "steelblue": "#4682B4",
-    "tan": "#D2B48C",
-    "teal": "#008080",
-    "thistle": "#D8BFD8",
-    "tomato": "#FF6347",
-    "turquoise": "#40E0D0",
-    "violet": "#EE82EE",
-    "wheat": "#F5DEB3",
-    "white": "#FFFFFF",
-    "whitesmoke": "#F5F5F5",
-    "yellow": "#FFFF00",
-    "yellowgreen": "#9ACD32",
-}
+class ColorName(StrEnum):
+    """Color name enumeration with hex values."""
+
+    ALICEBLUE = "#F0F8FF"
+    ANTIQUEWHITE = "#FAEBD7"
+    AQUA = "#00FFFF"
+    AQUAMARINE = "#7FFFD4"
+    AZURE = "#F0FFFF"
+    BEIGE = "#F5F5DC"
+    BISQUE = "#FFEBCD"
+    BLACK = "#000000"
+    BLANCHEDALMOND = "#FFEBCD"
+    BLUE = "#0000FF"
+    BLUEVIOLET = "#8A2BE2"
+    BROWN = "#A52A2A"
+    BURLYWOOD = "#DEB887"
+    CADETBLUE = "#5F9EA0"
+    CHARTREUSE = "#7FFF00"
+    CHOCOLATE = "#D2691E"
+    CORAL = "#FF7F50"
+    CORNFLOWERBLUE = "#6495ED"
+    CORNSILK = "#FFF8DC"
+    CRIMSON = "#DC143C"
+    CYAN = "#00FFFF"
+    DARKBLUE = "#00008B"
+    DARKCYAN = "#008B8B"
+    DARKGOLDENROD = "#B8860B"
+    DARKGRAY = "#A9A9A9"
+    DARKGREEN = "#006400"
+    DARKGREY = "#A9A9A9"
+    DARKKHAKI = "#BDB76B"
+    DARKMAGENTA = "#8B008B"
+    DARKOLIVEGREEN = "#556B2F"
+    DARKORANGE = "#FF8C00"
+    DARKORCHID = "#9932CC"
+    DARKRED = "#8B0000"
+    DARKSALMON = "#E9967A"
+    DARKSEAGREEN = "#8FBC8F"
+    DARKSLATEBLUE = "#483D8B"
+    DARKSLATEGRAY = "#2F4F4F"
+    DARKSLATEGREY = "#2F4F4F"
+    DARKTURQUOISE = "#00CED1"
+    DARKVIOLET = "#9400D3"
+    DEEPPINK = "#FF1493"
+    DEEPSKYBLUE = "#00BFFF"
+    DIMGRAY = "#696969"
+    DIMGREY = "#696969"
+    DODGERBLUE = "#1E90FF"
+    FIREBRICK = "#B22222"
+    FLORALWHITE = "#FFFAF0"
+    FORESTGREEN = "#228B22"
+    FUCHSIA = "#FF00FF"
+    GAINSBORO = "#DCDCDC"
+    GHOSTWHITE = "#F8F8FF"
+    GOLD = "#FFD700"
+    GOLDENROD = "#DAA520"
+    GRAY = "#808080"
+    GREEN = "#008000"
+    GREENYELLOW = "#ADFF2F"
+    GREY = "#808080"
+    HONEYDEW = "#F0FFF0"
+    HOTPINK = "#FF69B4"
+    INDIANRED = "#CD5C5C"
+    INDIGO = "#4B0082"
+    IVORY = "#FFFFF0"
+    KHAKI = "#F0E68C"
+    LAVENDER = "#E6E6FA"
+    LAVENDERBLUSH = "#FFF0F5"
+    LAWNGREEN = "#7CFC00"
+    LEMONCHIFFON = "#FFFACD"
+    LIGHTBLUE = "#ADD8E6"
+    LIGHTCORAL = "#F08080"
+    LIGHTCYAN = "#E0FFFF"
+    LIGHTGOLDENRODYELLOW = "#FAFAD2"
+    LIGHTGRAY = "#D3D3D3"
+    LIGHTGREEN = "#90EE90"
+    LIGHTGREY = "#D3D3D3"
+    LIGHTPINK = "#FFB6C1"
+    LIGHTSALMON = "#FFA07A"
+    LIGHTSEAGREEN = "#20B2AA"
+    LIGHTSKYBLUE = "#87CEFA"
+    LIGHTSLATEGRAY = "#778899"
+    LIGHTSLATEGREY = "#778899"
+    LIGHTSTEELBLUE = "#B0C4DE"
+    LIGHTYELLOW = "#FFFFE0"
+    LIME = "#00FF00"
+    LIMEGREEN = "#32CD32"
+    LINEN = "#FAF0E6"
+    MAGENTA = "#FF00FF"
+    MAROON = "#800000"
+    MEDIUMAQUAMARINE = "#66CDAA"
+    MEDIUMBLUE = "#0000CD"
+    MEDIUMORCHID = "#BA55D3"
+    MEDIUMPURPLE = "#9370DB"
+    MEDIUMSEAGREEN = "#3CB371"
+    MEDIUMSLATEBLUE = "#7B68EE"
+    MEDIUMSPRINGGREEN = "#00FA9A"
+    MEDIUMTURQUOISE = "#48D1CC"
+    MEDIUMVIOLETRED = "#C71585"
+    MIDNIGHTBLUE = "#191970"
+    MINTCREAM = "#F5FFFA"
+    MISTYROSE = "#FFE4E1"
+    MOCCASIN = "#FFE4B5"
+    NAVAJOWHITE = "#FFDEAD"
+    NAVY = "#000080"
+    OLDLACE = "#FDF5E6"
+    OLIVE = "#808000"
+    OLIVEDRAB = "#6B8E23"
+    ORANGE = "#FFA500"
+    ORANGERED = "#FF4500"
+    ORCHID = "#DA70D6"
+    PALEGOLDENROD = "#EEE8AA"
+    PALEGREEN = "#98FB98"
+    PALETURQUOISE = "#AFEEEE"
+    PALEVIOLETRED = "#DB7093"
+    PAPAYAWHIP = "#FFEFD5"
+    PEACHPUFF = "#FFDAB9"
+    PERU = "#CD853F"
+    PINK = "#FFC0CB"
+    PLUM = "#DDA0DD"
+    POWDERBLUE = "#B0E0E6"
+    PURPLE = "#800080"
+    REBECCAPURPLE = "#663399"
+    RED = "#FF0000"
+    ROSYBROWN = "#BC8F8F"
+    ROYALBLUE = "#4169E1"
+    SADDLEBROWN = "#8B4513"
+    SALMON = "#FA8072"
+    SANDYBROWN = "#F4A460"
+    SEAGREEN = "#2E8B57"
+    SEASHELL = "#FFF5EE"
+    SIENNA = "#A0522D"
+    SILVER = "#C0C0C0"
+    SKYBLUE = "#87CEEB"
+    SLATEBLUE = "#6A5ACD"
+    SLATEGRAY = "#708090"
+    SLATEGREY = "#708090"
+    SNOW = "#FFFAFA"
+    SPRINGGREEN = "#00FF7F"
+    STEELBLUE = "#4682B4"
+    TAN = "#D2B48C"
+    TEAL = "#008080"
+    THISTLE = "#D8BFD8"
+    TOMATO = "#FF6347"
+    TURQUOISE = "#40E0D0"
+    VIOLET = "#EE82EE"
+    WHEAT = "#F5DEB3"
+    WHITE = "#FFFFFF"
+    WHITESMOKE = "#F5F5F5"
+    YELLOW = "#FFFF00"
+    YELLOWGREEN = "#9ACD32"
+
 
 COLOR_PALETTE = [
     "#1f77b4",
